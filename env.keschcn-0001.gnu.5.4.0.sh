6339517aeec1da304b626aba79efa5b0e1b0bf98
Pascal Spörri <pspoerri@users.noreply.github.com>
2017-10-26 09:24:20 +0200

    Update boost path for GNU on Kesch-test

--- env.keschcn-0001.gnu.5.4.0.sh
+++ env.keschcn-0001.gnu.5.4.0.sh
@@ -19,7 +19,7 @@ export LINKER_X86_64=$(which ld)
 export LDFLAGS="-L$EBROOTGCC/lib64 ${LDFLAGS}"
 export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
 
-export BOOST_ROOT=/apps/escha/UES/jenkins/RH7.3-gnu_PE17.02/easybuild/software/boost/1.63.0-gmvolf-17.02-python-2.7.13
+export BOOST_ROOT=/project/c14/install/kesch/boost/boost_1_65_1/
 export LD_LIBRARY_PATH=${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}
 
 # We have gcc for gnu, cray and pgi environments
