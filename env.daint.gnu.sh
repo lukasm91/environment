1ba6027f66cfaab938c99b861fb76131b273cbea
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2017-10-27 10:04:12 +0200

    Move boost on daint to /scratch

--- env.daint.gnu.sh
+++ env.daint.gnu.sh
@@ -10,7 +10,7 @@ module load craype-accel-nvidia60
 module load CMake
 
 # Boost
-export BOOST_ROOT=/project/c14/install/daint/boost/boost_1_64_0/
+export BOOST_ROOT=/scratch/snx3000/jenkins/install/boost/boost_1_64_0
 export LD_LIBRARY_PATH=${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}
 
 # Override C++ and C compiler
