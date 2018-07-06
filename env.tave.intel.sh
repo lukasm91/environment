3d0ce985da91260e28835f12b7da07dd29420833
Felix Thaler <felix.thaler@nummi.ch>
2018-07-06 15:08:12 +0200

    Tave Intel environment (#4)
    Add an Intel environment for Tavé and further fixes MPI compilation for the Tavé GNU environment.

--- env.tave.intel.sh
+++ env.tave.intel.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 prgenv=`module list -t 2>&1 | grep 'PrgEnv-'`
 if [ -z "${prgenv}" ]; then
-    module load PrgEnv-gnu
+    module load PrgEnv-intel
 else
-    module swap ${prgenv} PrgEnv-gnu
+    module swap ${prgenv} PrgEnv-intel
 fi
-
+module swap intel intel/18.0.2.199
+module load gcc # for recent STL
 module load cray-mpich
+
 module load CMake
 
 # Boost
 export BOOST_ROOT=/scratch/snx2000/jenkins/install/boost/boost_1_64_0
 export LD_LIBRARY_PATH=${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}
 
-export FC=ftn
+export FC=`which gfortran`
